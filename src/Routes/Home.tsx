864064ede3a71e9a648b04e455cc0f684f8f9fd1
Jho2e <ohjaehokor@naver.com>
2022-11-03 16:11:24 +0900

    Add: Home.tsx 작업
    배경 포스터, 타이틀, 설명,
    슬라이더 생성.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,107 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import styled from 'styled-components';
+import { getMovies, IGetMoviesResult } from '../api';
+import { makeImagePath } from '../utils';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const Wrapper = styled.div`
+  background-color: black;
+  overflow-x: hidden;
+`;
+const Loader = styled.div`
+  height: 20vh;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+const Title = styled.h2`
+  font-size: 68px;
+  //  white-space: nowrap;
+  margin-bottom: 20px;
+`;
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  width: 100%;
+
+  position: absolute;
+`;
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 100px;
+  color: red;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth - 460,
+  },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth + 460 },
+};
+
 function Home() {
-  return <div style={{ backgroundColor: 'white', height: '200vh' }}></div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ['movies', 'nowPlaying'],
+    getMovies
+  );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => prev + 1);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: 'tween', duration: 2 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
